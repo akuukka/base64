b05cfdfd52bf7ecc723a139ebcf457348aa9c53c
Antti Kuukka <anttiku@protonmail.com>
2023-01-22 13:19:36 +0200

    Public function for retrieving number of bytes represented by a base64 encoded string.
    This may be useful when you need to allocate a buffer for the data or check that an existing buffer is large enough.

--- source/Base64.hpp
+++ source/Base64.hpp
@@ -52,49 +52,64 @@ inline int shift(int v, int n)
     return v >> -n;
 }
 
-inline size_t decode(const char* data, size_t count, void* out)
+/// Return number of bytes represented by a base64 encoded string.
+inline size_t dataLength(const char* base64String, size_t length, size_t* paddingOut = nullptr)
 {
+    const size_t padding =
+        (length >= 1 && base64String[length - 1] == '=') ?
+        (length >= 2 && base64String[length - 2] == '=' ? 2 : 1) : 0;
+    const size_t bits = length ? length * 6 - padding * 8 : 0;
+    const size_t bytes = bits / 8;
+    if (paddingOut) {
+        *paddingOut = padding;
+    }
+    return bytes;
+}
+
+inline size_t decode(const char* str, size_t length, void* out)
+{
+    const unsigned char* data = reinterpret_cast<const unsigned char*>(str);
     unsigned char* r = static_cast<unsigned char*>(out);
-    const int table[] = {
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1,
-        -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-        22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-        38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+    const unsigned char table[] = {
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 62, 255, 255, 255, 63, 52, 53, 54, 55,
+        56, 57, 58, 59, 60, 61, 255, 255, 255, 255, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+        9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255,
+        255, 255, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+        45, 46, 47, 48, 49, 50, 51, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255
     };
-    if (!(count % 4 == 0)) {
+    if (!(length % 4 == 0)) {
         throw InvalidEncoding("Padding expected");
     }
-    const size_t padding =
-        (count >= 1 && data[count - 1] == '=') ?
-        (count >= 2 && data[count - 2] == '=' ? 2 : 1) : 0;
-    const size_t bits = count ? count * 6 - padding * 8 : 0;
-    const size_t bytes = bits / 8;
-    const size_t sextets = count - padding;
+    size_t padding;
+    const size_t bytes = dataLength(str, length, &padding);
+    const size_t sextets = length - padding;
     const unsigned char masks[] = { 3, 192, 240, 252 };
     for (size_t i=0; i < sextets; i++) {
-        const char c = data[i];
-        if (table[c] == -1) {
-            throw InvalidEncoding((std::string("Unexpected character in encoding: ") + c).c_str());
+        const unsigned char c = table[data[i]];
+        if (c == 255) {
+            throw InvalidEncoding("Unexpected character in encoding");
         }
         const size_t beginBit = i * 6;
         const size_t endBit = (i+1) * 6;
         const size_t beginByte = beginBit / 8;
         const size_t endByte = endBit / 8;
         const int beginOffset = beginBit % 8;
-        const int endOffset = endBit % 8;
         const unsigned char mask1  = masks[beginOffset/2];
-        r[beginByte] = r[beginByte] & mask1 | shift(table[c], 2 - beginOffset);
+        r[beginByte] = r[beginByte] & mask1 | shift(c, 2 - beginOffset);
         if (beginByte != endByte && endByte < bytes) {
+            const int endOffset = endBit % 8;
             const unsigned char mask2 = ~(((1 << (8-endOffset)) - 1) << endOffset);
-            r[endByte] = r[endByte] & mask2 | (table[c] << (8-endOffset));
+            r[endByte] = r[endByte] & mask2 | (c << (8-endOffset));
         }
     }
     return bytes;
@@ -108,13 +123,8 @@ inline size_t decode(const std::string& base64, void* out)
 template<typename T>
 inline T decode(const char* data, size_t count)
 {
-    const size_t padding =
-        (count >= 1 && data[count - 1] == '=') ?
-        (count >= 2 && data[count - 2] == '=' ? 2 : 1) : 0;
-    const size_t bits = count ? count * 6 - padding * 8 : 0;
-    const size_t bytes = bits / 8;
     T r;
-    r.resize(bytes);
+    r.resize(dataLength(data, count));
     decode(data, count, &r[0]);
     return r;
 }
