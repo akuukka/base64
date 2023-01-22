107cfe9bdd11defdff017212689974bb6d1c32b8
Antti Kuukka <anttiku@protonmail.com>
2023-01-22 06:56:23 +0200

    Check once

--- source/Base64.hpp
+++ source/Base64.hpp
@@ -50,7 +50,7 @@ inline size_t decode(const char* data, size_t count, void* out)
     const int table[] = {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1,
+        -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 255, -1,
         -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
         22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
         38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -69,15 +69,15 @@ inline size_t decode(const char* data, size_t count, void* out)
         (count >= 2 && data[count - 2] == '=' ? 2 : 1) : 0;
     const size_t bits = count ? count * 6 - padding * 8 : 0;
     const size_t bytes = bits / 8;
-    for (size_t i=0;i<count;i++) {
+    for (size_t i=0; i<count; i++) {
         const char c = data[i];
-        for (size_t j=0;j<6;j++) {
+        if (table[c] == -1) {
+            throw InvalidEncoding((std::string("Unexpected character in encoding: ") + c).c_str());
+        }
+        for (size_t j=0; j<6; j++) {
             const int bit = i * 6 + j;
             if (bit >= bits) {
                 break;
-            }
-            if (table[c] == -1) {
-                throw InvalidEncoding("Unexpected character in encoding.");
             }
             if (table[c] & (1 << (5-j))) {
                 r[bit/8] = r[bit/8] | (1 << (7-bit%8));
